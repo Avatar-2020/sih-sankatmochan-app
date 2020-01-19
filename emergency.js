bf740f2c36da823048df7cc8ae3bc95c15daffbb
aang <devanshguptamrt@gmail.com>
2020-01-19 10:58:02 +0530

    sending of payload to cloud included

--- emergency.js
+++ emergency.js
@@ -3,17 +3,21 @@
 import React, { Component } from "react";
 
 
-import { View, TouchableOpacity, Text } from 'react-native';
+import { View,  Text } from 'react-native';
 import { Camera } from "expo-camera";
 import * as Location from "expo-location";
 import * as Permission from "expo-permissions";
 import * as FileSystem from "expo-file-system";
 import { Headline } from "react-native-paper";
 import { Button } from "react-native-paper";
+
+
+const STAGE = "dev";
+
 export default class EmergencyWindow extends Component {
     constructor(props) {
         super(props);
-        this.state = {  statusText:"" , disabled: false, btnText: "Panic Situation" }
+        this.state = { statusText: "", disabled: false, btnText: "Panic Situation" }
     }
 
     async askForPermission() {
@@ -46,31 +50,55 @@ export default class EmergencyWindow extends Component {
         Location.getCurrentPositionAsync({ enableHighAccuracy: true })
             .then(position => {
                 console.log("Position: ", position);
-                this.setState({position});
+                this.setState({ position });
             }).catch(error => {
                 console.log("Error Code : ", error.code, "Error Message : ", error.message);
             });
 
         //Getting device info    
-            FileSystem.readAsStringAsync(FileSystem.documentDirectory+"/dev.json",{encoding:FileSystem.EncodingType.UTF8})
-            .then(content=>{
-                this.setState({deviceInfo:JSON.parse(content)});
+        FileSystem.readAsStringAsync(FileSystem.documentDirectory + "/dev.json", { encoding: FileSystem.EncodingType.UTF8 })
+            .then(content => {
+                this.setState({ deviceId: JSON.parse(content) });
             })
 
 
         if (this.camera) {
             const cam = this.camera;
-            this.setState({statusText:"Video Recording started"});
+            this.setState({ statusText: "Video Recording started" });
             cam.recordAsync({
                 maxDuration: 25, //25 secs
                 mute: true
-            }).then(uri => {
+            }).then(async uri => {
                 console.log("Cached file Info : ", uri);
-                this.setState({statusText:"Video Recorded"});
-                fetch("url",{
-                    method:"post",
-                    
-                })
+                this.setState({ statusText: "Video Recorded" });
+                const content = await FileSystem.readAsStringAsync(uri, { encoding: FileSystem.EncodingType.Base64 });
+                const payload = {
+                    loc: this.state.position,
+                    deviceId: this.state.deviceId,
+                    video: content
+                }
+                console.log("Sending all the data in cloud");
+                this.setState({ statusText: "Sending data in cloud" });
+                if (STAGE !== "dev") {
+                    fetch("url", {
+                        method: "post",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: payload
+                    }).then(r => {
+                        if (r.status === 200) {
+
+                            return r.json();
+                        }
+                    }).then(r => {
+                        console.log("Accident Recorded at the backend : Response : ", r);
+                        this.setState({ statusText: "Accident Recorded" });
+                    }).catch(err => {
+                        console.log("Error occured while sending data to the cloud : ", err.code, err.message);
+                    })
+                }
+
             }).catch(err => {
                 console.log("Error Report Occured : ", err.code, " : ", err.message);
 
